2759fd015e5f7f9a086da891d89207429ef96430
Basso-Giovanni <119098857+Basso-Giovanni@users.noreply.github.com>
2025-04-28 12:58:37 +0200

    Migrazione a MySQL

--- backend_triptales/settings.py
+++ backend_triptales/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'backend_triptales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pwtriptales_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',  # o l'indirizzo del server MySQL
+        'PORT': '3306',       # default di MySQL
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
