bc3a03d1a4444f3b42b7f75c344afb569be4f3df
Basso-Giovanni <119098857+Basso-Giovanni@users.noreply.github.com>
2025-05-09 09:28:13 +0200

    Risolto problema aggiornamento password

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,7 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 
 CustomUser = get_user_model()
@@ -10,11 +13,9 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        # Verifica che 'username' sia presente nei dati
         if 'username' not in validated_data:
             raise serializers.ValidationError({"username": "Questo campo è obbligatorio"})
 
-        # Crea l'utente
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
@@ -23,3 +24,30 @@ class UserSerializer(serializers.ModelSerializer):
             avatar=validated_data.get('avatar', None)
         )
         return user
+
+    def update(self, instance, validated_data):
+        # Gestione separata della password per assicurarne la cifratura
+        password = validated_data.pop('password', None)
+
+        # Aggiorna gli altri campi normalmente
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
+
+class UpdateProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, *args, **kwargs):
+        user = request.user
+        serializer = UserSerializer(user, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
