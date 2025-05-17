d8205e5c6c5fe84dc5346283c9aeb4ae4d1cfd52
Basso-Giovanni <119098857+Basso-Giovanni@users.noreply.github.com>
2025-05-17 08:32:21 +0200

    Aggiunto id utente nei commenti

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Comment
