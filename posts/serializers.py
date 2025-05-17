d8a7d194a7dc2de564030dfbd690744bae6dde77
Basso-Giovanni <119098857+Basso-Giovanni@users.noreply.github.com>
2025-05-17 08:24:34 +0200

    Aggiunto array id like

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,14 +4,15 @@ from trips.models import TripGroup
 
 class PostSerializer(serializers.ModelSerializer):
     likes_count = serializers.SerializerMethodField()
+    liked_user_ids = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = [
             'id', 'title', 'description', 'created_at',
-            'created_by', 'image', 'group', 'likes_count'
+            'created_by', 'image', 'group', 'likes_count', 'liked_user_ids'
         ]
-        read_only_fields = ['id', 'created_by', 'created_at', 'likes_count']
+        read_only_fields = ['id', 'created_by', 'created_at', 'likes_count', 'liked_user_ids']
 
     def get_likes_count(self, obj):
         return obj.likes.count()
@@ -20,3 +21,6 @@ class PostSerializer(serializers.ModelSerializer):
         if not TripGroup.objects.filter(id=value.id).exists():
             raise serializers.ValidationError("Il gruppo specificato non esiste.")
         return value
+
+    def get_liked_user_ids(self, obj):
+        return list(obj.likes.values_list('id', flat=True))
